dd1bc9c62351c958d90872fbf1961c1d3d160b5f
Colin Lacey <celestial_doom@derpymail.org>
2018-05-08 21:05:12 +0100

    Improved the UI

--- SlimShopper UWP/MainPage.xaml.vb
+++ SlimShopper UWP/MainPage.xaml.vb
@@ -1,6 +1,7 @@
 ﻿' The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 Imports Windows.Phone.UI.Input
+Imports Windows.UI.Popups
 
 ''' <summary>
 ''' An empty page that can be used on its own or navigated to within a Frame.
@@ -8,30 +9,58 @@ Imports Windows.Phone.UI.Input
 Public NotInheritable Class MainPage
     Inherits Page
 
-    Public AppName As String = Package.Current.DisplayName
-    Public view = ApplicationView.GetForCurrentView()
-
     Async Sub BackPressed(sender As Object, e As BackPressedEventArgs)
         'Handles any Back button presses.
         e.Handled = True
-        If wv.CanGoBack Then
-            wv.GoBack()
-        Else
-            Await displayMessageAsync(AppName, "Are you sure you want to exit the app?", "")
-        End If
+        Await displayMessageAsync(AppName, "Are you sure you want to exit the app?", "", True)
     End Sub
+
+    'displayMessageAsync displays a message, either a Notification, or a simple message dialog. Using 'notification' in the dialogType will make the dialog a notification. Leaving it blank will display a message dialog.
+    Public Async Function displayMessageAsync(ByVal title As String, ByVal content As String, ByVal dialogType As String, ByVal warning As Boolean) As Task
+        Dim messageDialog = New MessageDialog(content, title)
+        If dialogType = "notification" Then
+        Else
+            messageDialog.Commands.Add(New UICommand("Yes", Nothing))
+            messageDialog.Commands.Add(New UICommand("No", Nothing))
+            messageDialog.DefaultCommandIndex = 0
+        End If
+        messageDialog.CancelCommandIndex = 1
+        Dim cmdResult = Await messageDialog.ShowAsync()
+        If cmdResult.Label = "Yes" Then
+            If warning = False Then
+                view.ExitFullScreenMode()
+                localSettings.Values("FULLSCREEN") = "0"
+                togg_FS.IsOn = False
+                Sett.Visibility = Visibility.Collapsed
+            Else
+                Application.Current.Exit()
+            End If
+        End If
+    End Function
 
     Private Sub About_Click(sender As Object, e As RoutedEventArgs) Handles About.Click
         CloseMenu()
         ShowSettings()
     End Sub
 
+    Private Sub AppSettings_Click(sender As Object, e As RoutedEventArgs) Handles AppSettings.Click
+        CloseMenu()
+        Sett.Visibility = Visibility.Visible
+    End Sub
+
     Private Async Sub BFS_Click(sender As Object, e As RoutedEventArgs) Handles BFS.Click
+        CLOSEALL()
         If wv.CanGoBack Then
             wv.GoBack()
         Else
-            Await displayMessageAsync(AppName, "Are you sure you want to exit the app?", "")
+            Await displayMessageAsync(AppName, "Are you sure you want to exit the app?", "", True)
         End If
+    End Sub
+
+    Private Sub CLOSEALL()
+        Sett.Visibility = Visibility.Collapsed
+        Menu.Visibility = Visibility.Collapsed
+        Info.Visibility = Visibility.Collapsed
     End Sub
 
     Private Sub CloseGrid_Click(sender As Object, e As RoutedEventArgs) Handles CloseGrid.Click
@@ -48,6 +77,10 @@ Public NotInheritable Class MainPage
 
     Private Sub CM_Click(sender As Object, e As RoutedEventArgs) Handles CM.Click
         Menu.Visibility = Visibility.Collapsed
+    End Sub
+
+    Private Sub CS_Click(sender As Object, e As RoutedEventArgs) Handles CS.Click
+        Sett.Visibility = Visibility.Collapsed
     End Sub
 
     Private Async Sub hyperDev_Click(sender As Object, e As RoutedEventArgs) Handles hyperDev.Click
@@ -77,6 +110,17 @@ Public NotInheritable Class MainPage
         Menu.Visibility = Visibility.Collapsed
         Info.Visibility = Visibility.Collapsed
         Me.InitializeComponent()
+        If SetFullScreen Is Nothing Then
+            localSettings.Values("FullScreen") = "0"
+        Else
+            If SetFullScreen = "0" Then
+                view.ExitFullScreenMode()
+                togg_FS.IsOn = False
+            Else
+                view.TryEnterFullScreenMode()
+                togg_FS.IsOn = True
+            End If
+        End If
         Dim statusbar = "Windows.UI.ViewManagement.StatusBar"
         If Windows.Foundation.Metadata.ApiInformation.IsTypePresent(statusbar) Then
             Await Windows.UI.ViewManagement.StatusBar.GetForCurrentView().HideAsync
@@ -90,7 +134,7 @@ Public NotInheritable Class MainPage
             CountryValue = Stored_ID
         End If
         Dim value As Object = localSettings.Values("STORED_COUNTRY")
-        URLBuilder = "www.amazon." & CountryURL(value)
+        URLBuilder = "https://www.amazon." & CountryURL(value) & "/"
         wv.Source = New Uri("https://www.amazon." & CountryURL(value) & "/")
         Prog_Ring.IsActive = True
     End Sub
@@ -117,6 +161,13 @@ Public NotInheritable Class MainPage
         wv.Source = New Uri("https://www.amazon." & CountryURL(selectedValue) & "/gp/css/order-history/ref=nav_youraccount_order")
     End Sub
 
+    Private Async Sub RadioReset_Checked(sender As Object, e As RoutedEventArgs) Handles RadioReset.Checked
+        Dim rb As RadioButton = TryCast(sender, RadioButton)
+        If rb IsNot Nothing Then
+            Await displayMessageAsync(AppName, "This will reset all of your app settings." & vbCrLf & "Are you sure you want to continue?", "", False)
+        End If
+    End Sub
+
     Private Sub Settings_Click(sender As Object, e As RoutedEventArgs) Handles Settings.Click
         CloseMenu()
         CountryChoice.Visibility = Visibility.Visible
@@ -134,8 +185,27 @@ Public NotInheritable Class MainPage
     End Sub
 
     Private Async Sub TFS_Click(sender As Object, e As RoutedEventArgs) Handles TFS.Click
+        CLOSEALL()
         Dim ScrollToTopString = "var int = setInterval(function(){window.scrollBy(0, -36); if( window.pageYOffset === 0 ) clearInterval(int); }, 0.0);"
         Await wv.InvokeScriptAsync("eval", New String() {ScrollToTopString})
+    End Sub
+
+    Private Sub togg_FS_Toggled(sender As Object, e As RoutedEventArgs) Handles togg_FS.Toggled
+        Sett.Visibility = Visibility.Collapsed
+        Dim toggleSwitch As ToggleSwitch = TryCast(sender, ToggleSwitch)
+        If toggleSwitch IsNot Nothing Then
+            If toggleSwitch.IsOn = True Then
+                view.TryEnterFullScreenMode()
+                localSettings.Values("FULLSCREEN") = "1"
+            Else
+                view.ExitFullScreenMode()
+                localSettings.Values("FULLSCREEN") = "0"
+            End If
+        End If
+    End Sub
+
+    Private Sub wv_GotFocus(sender As Object, e As RoutedEventArgs) Handles wv.GotFocus
+        CLOSEALL()
     End Sub
 
     Private Sub wv_LoadCompleted(sender As Object, e As NavigationEventArgs) Handles wv.LoadCompleted
@@ -150,7 +220,8 @@ Public NotInheritable Class MainPage
     End Sub
 
     Private Async Sub XFS_Click(sender As Object, e As RoutedEventArgs) Handles XFS.Click
-        Await displayMessageAsync(AppName, "Are you sure you want to exit the app?", "")
+        CLOSEALL()
+        Await displayMessageAsync(AppName, "Are you sure you want to exit the app?", "", True)
     End Sub
 
 End Class
--- SlimShopper UWP/Vars.vb
+++ SlimShopper UWP/Vars.vb
@@ -3,11 +3,12 @@
 'The 'Vars.vb' file contains the public variables use in the app
 
 Module Vars
-    Public AccessWhat As String = "their Amazon account"
-    Public Company As String = "Amazon, Inc."
 
-    Public AboutInfo As String = AppName & " is a UWP app for Windows 10 Mobile to allow the user to access " & AccessWhat & " from a single portal." & vbCrLf & vbCrLf & "Small memory footprint, open source, and forever free." & vbCrLf & vbCrLf & "This app is NOT associated in ANY way with " & Company
+    'The following public variables will be documented soon.
+
+    Public AccessWhat As String = "their Amazon account"
     Public AppName As String = Package.Current.DisplayName
+    Public Company As String = "Amazon, Inc."
     Public CountryName As String() = {"United Kingdom", "United States", "Australia", "Brazil", "Canada", "China", "France", "Germany", "India", "Italy", "Japan", "Mexico", "Netherlands", "Spain"}
     Public CountryURL As String() = {"co.uk", "com", "com.au", "com.br", "ca", "cn", "fr", "de", "in", "it", "co.jp", "com.mx", "nl", "es"}
     Public CountryValue As String
@@ -15,33 +16,16 @@ Module Vars
     Public CurrentWidth As Double
     Public localSettings As Windows.Storage.ApplicationDataContainer = Windows.Storage.ApplicationData.Current.LocalSettings
     Public PrivacyInfo As String = AppName & " Privacy" & vbCrLf & vbCrLf & "----------" & vbCrLf & vbCrLf & "No personal, or private, information of either you, or your device, is collected by this app." & vbCrLf & vbCrLf & "Neither is ANY information transmitted by this app."
+    Public AboutInfo As String = AppName & " is a UWP app for Windows 10 Mobile to allow the user to access " & AccessWhat & " from a single portal." & vbCrLf & vbCrLf & "Small memory footprint, open source, and forever free." & vbCrLf & vbCrLf & "This app is NOT associated in ANY way with " & Company
     Public Stored_ID As Object = localSettings.Values("STORED_COUNTRY")
+    Public SetFullScreen As Object = localSettings.Values("FULLSCREEN")
     Public StoredCountry As String
     Public URLBuilder As String
     Public view = ApplicationView.GetForCurrentView()
 
-    Public Async Function displayMessageAsync(ByVal title As String, ByVal content As String, ByVal dialogType As String) As Task
-        Dim messageDialog = New MessageDialog(content, title)
-        If dialogType = "notification" Then
-        Else
-            messageDialog.Commands.Add(New UICommand("Yes", Nothing))
-            messageDialog.Commands.Add(New UICommand("No", Nothing))
-            messageDialog.DefaultCommandIndex = 0
-        End If
-        messageDialog.CancelCommandIndex = 1
-        Dim cmdResult = Await messageDialog.ShowAsync()
-        If cmdResult.Label = "Yes" Then
-            Application.Current.Exit()
-        End If
-    End Function
-
+    'LoadCountries loads the list of countries into the lstCountry listbox.
     Public Function LoadCountries()
         Return CountryName
     End Function
-
-    Public Async Sub SimpleMessageDialog(ByVal message As String, ByVal header As String)
-        Dim dialog = New MessageDialog(header, message)
-        Await dialog.ShowAsync()
-    End Sub
 
 End Module
